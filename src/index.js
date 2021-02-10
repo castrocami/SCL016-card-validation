a10039f877c68d4437d1cf20c03fcf88a60835b0
Camila Castro <ccastromn@gmail.com>
2021-02-10 15:56:32 -0300

    show tc whith dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //Esto es lo que importa el js donde hago el método que valida la tc
 import validator from './validator.js';
 
-//Funcion que tiene todos los listeners de los botones
+//Función que tiene todos los listeners de los botones
  const addButtonsListeners = () => {
      //Listener de boton agregar nueva tarjeta
     const firstButtonClick = document.getElementById("newCardButton");
@@ -15,19 +15,19 @@ import validator from './validator.js';
     
  }
 
- //Funcion que va desde login a home
+ //Función que va desde login a home
  const home = () =>{
      document.getElementById("login").style.display = "none";
      document.getElementById("home").style.display = "initial";
 
  }
- //Funcion que me muestra formulario para numeros de tarjetas
+ //Función que me muestra formulario para numeros de tarjetas
  const formToComplete = () =>{
     document.getElementById("home").style.display = "none";
     document.getElementById("newCard").style.display = "initial";
  }
 
-//Funcion que se devuelve al homepage y procesa la funcion valid card
+//Función que se devuelve al homepage y procesa la funcion valid card
  const backToHomepage = () =>{
      //Valor de cc
     let creditCardNumber = document.getElementById("ccNumber").value;
@@ -42,31 +42,46 @@ import validator from './validator.js';
             cvv: document.getElementById("ccCVV").value,
         };
         saveTheinformation(walletAcces); 
+        showWallet();
         document.getElementById("newCard").style.display="none";
         document.getElementById("home").style.display="initial";
     }else {
         alert ("Por favor ingrese un número de tarjeta válido");
         
-    }
-   
-    //console mientras tanto...
-    console.log(validator.maskify(creditCardNumber));
-    
+    } 
  }
 
-    //Funcion que guarda los input del local storage
-    const saveTheinformation = (walletAcces) => {
-        let walletArray = [];
-        if (localStorage.getItem("wallet")== null){
-            walletArray.push(walletAcces);
-            localStorage.setItem("wallet", JSON.stringify(walletArray));
-        } else {
+ //Función que guarda el objeto en el local storage
+const saveTheinformation = (walletAcces) => {
+    let walletArray = [];
+    if (localStorage.getItem("wallet")== null){
+        walletArray.push(walletAcces);
+        localStorage.setItem("wallet", JSON.stringify(walletArray));
+    } else {
         walletArray = JSON.parse(localStorage.getItem("wallet"));  
         walletArray.push(walletAcces);
         localStorage.setItem("wallet", JSON.stringify(walletArray));
         }
-    };
+}
 
+//Función que toma el arreglo del l.s y lo muestra en home (Acá llamo a la funcion maskify!)
+const showWallet = () =>{
+    let getWallet = localStorage.getItem("wallet");
+    getWallet = JSON.parse(getWallet);
+    console.log(getWallet.length);
+    let element = document.getElementById("cardsContainer");
+    element.innerHTML = "";
+    for (let i = 0; i < getWallet.length; i++){
+        let one = document.createElement("li");
+        one.classList.add("cardSummary");
+        let two = document.createTextNode(getWallet[i].alias+ " :" + validator.maskify(getWallet[i].number));
+        one.appendChild(two);
+        element.appendChild(one);      
+    }
+
+    
+
+}
  
  
 
@@ -75,10 +90,8 @@ import validator from './validator.js';
 
 
 //Codigo que se llama siempre:
-
-//Llamo a la funcion click
-addButtonsListeners()
-
+addButtonsListeners();
+showWallet();
 
 
 
